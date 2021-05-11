f9f0be2340f2a31d139fe7ab7c4a80e7140c899b
Dmitry Dzakhov <dmitrydzz@robot-mitya.ru>
2021-05-11 20:45:18 +0300

    Prepare 1.0.2

--- keyzz/main.cpp
+++ keyzz/main.cpp
@@ -22,7 +22,7 @@ using minunity::ColorInfo;
 using minunity::ColliderGroups;
 using minunity::Graph;
 
-const char VERSION[] = "1.0.1";
+const char VERSION[] = "1.0.2";
 const char INTRO[] = "keyzz© terminal game by @DmitryDzz";
 
 void output_intro() {
